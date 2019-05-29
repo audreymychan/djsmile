1888e45d7b7de5592f317e5602734a8ab4cea7c1
audreymychan <audreymychan@gmail.com>
2019-05-29 12:55:51 -0400

    Update flask app front end look and feel

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, Response
+from flask import Flask, request, render_template, Response, redirect, url_for
 from camera import Camera
 from generate_joke import get_joke
 
 app = Flask(__name__)
+
 
 def gen(camera):
     while True:
@@ -10,16 +11,23 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-joke = get_joke()
 
 @app.route('/')
 def index():
-    return render_template('index2.html', joke = joke)
+    return render_template('index.html', joke=get_joke())
+
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
+@app.route('/', methods=['POST'])
+def update():
+    if request.form['next'] == '▶':
+        return render_template('index.html', scroll='livefeed', joke=get_joke())
+
+
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1', debug = True, port = "5000")
+    app.run(host='127.0.0.1', debug=True, port="5000")
