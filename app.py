7fcb8d5da44df40b0f7e0fd4a30c0660c1dad239
audreymychan <audreymychan@gmail.com>
2019-06-09 21:26:19 -0400

    Remove unused imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, Response, redirect, url_for
+from flask import Flask, request, render_template, Response
 from camera import Camera
 from generate_joke import get_joke
 
